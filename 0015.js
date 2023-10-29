20361637621f52005c984980938ade74c2bbae8e
Enochmia <126403143+Enochmia@users.noreply.github.com>
2023-10-30 00:15:33 +0900

    Create 0015.js

---
+++ 0015.js
@@ -0,0 +1,34 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+huskylens.forgetLearn()
+huskylens.clearOSD()
+basic.pause(1000)
+huskylens.writeLearn1(1)
+led.plot(0, 0)
+let li: number;
+li = 0
+let xco: number;
+basic.forever(function () {
+    if (li == 0) {
+        basic.pause(5000)
+    }
+    li += 1
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock) == true) {
+        xco = huskylens.readeBox(1, Content1.xCenter)
+        led.plot(3, 0)
+        huskylens.clearOSD()
+        if (xco <= 159) {
+            huskylens.writeOSD("appeared left", 40, 30)
+        } else if (xco <= 319) {
+            huskylens.writeOSD("appeared right", 40, 30)
+        } else {
+            ;
+        }
+    } else {
+        huskylens.clearOSD()
+        basic.pause(2000)
+        huskylens.writeOSD("Didn't appeared", 150, 30)
+        led.plot(1, 0)
+    }
+})
